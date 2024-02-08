9b3902a2d96ad2697827aef55489fad817c1cc0d
Sean Estrella <sean.estrella2002@gmail.com>
2024-02-07 18:30:51 -0800

    Implement delete and get name and job

--- csc-307-app/packages/express-backend/backend.js
+++ csc-307-app/packages/express-backend/backend.js
@@ -48,22 +48,48 @@ const findUserByName = (name) => {
     );
 };
 
+const findUserByNameAndJob = (name, job) => {
+    return users["users_list"].filter(
+        (user) => user.name === name && user.job === job
+    );
+}
+
 const findUserById = (id) =>
     users["users_list"].find((user) => user["id"] === id);
+
+const findUserByJob = (job) =>
+    users.users_list.find(user => user["job"] === job)
 
 const addUser = (user) => {
     users["users_list"].push(user);
     return user;
 };
+const findUserIndexById = (id) => 
+    users.users_list.findIndex(user => user["id"] === id); 
+
+
 app.get("/users", (req, res) => {
     const name = req.query.name;
-    if (name != undefined) {
+    const job = req.query.job;
+    let modifiedUsers = users["users_list"]
+
+    if (name != undefined && job != undefined) {
+        let result = findUserByNameAndJob(name, job);
+        result = { users_list: result}
+        res.send(result);
+
+    } else if (name != undefined) {
         let result = findUserByName(name);
         result = { users_list: result };
         res.send(result);
+    } else if (job != undefined) {
+        let result = findUserByJob(job);
+        result = { users_list: result };
+        res.send(result);
+
     } else {
         res.send(users);
-    }
+    }    
 });
 
 app.get("/users/:id", (req, res) => {
@@ -75,6 +101,18 @@ app.get("/users/:id", (req, res) => {
         res.send(result);
     }
 });
+
+app.delete("/users/:id", (req, res) => {
+    const id  = req.params;
+    let index = findUserIndexById(id);
+    if (index !== -1) {
+        users.users_list.splice(index, 1); 
+        res.status(204).send(); 
+    } else {
+        res.status(404).send("Resource not found."); 
+    }
+});
+
 
 app.post("/users", (req, res) => {
     const userToAdd = req.body;
